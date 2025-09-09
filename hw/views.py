467f976fb21340be8833d52e6a8a910136334115
alvenie <alvenie@bu.edu>
2025-09-08 23:10:33 -0400

    Added more to the hw template.

--- hw/views.py
+++ hw/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
 import time
+import random
 
 # Create your views here.
 def home(request):
@@ -22,4 +23,12 @@ def home_page(request):
     '''Respond to the URL '', delegate work to a template.'''
 
     template_name = 'hw/home.html'
-    return render(request, template_name)   
+    # a dict of context variables (key-value pairs)
+    context = {
+        "time": time.ctime(),
+        "letter1": chr(random.randint(65,90)),
+        "letter2": chr(random.randint(65,90)),
+        "number": random.randint(1,10),
+        
+    }
+    return render(request, template_name, context)   
