ff68f394af7f614de997cd951374973f431846c7
alvenie <alvenie@bu.edu>
2025-09-25 11:06:56 -0400

    Created base template.

--- mini_insta/views.py
+++ mini_insta/views.py
@@ -1,3 +1,14 @@
+# mini_insta/views.py
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Profile
 
 # Create your views here.
+
+def ProfileListView(ListView):
+    '''Define a view class to show all mini insta Profiles.'''
+
+    model = Profile
+    template_name = 'mini_insta/show_all_profiles.html'
+    context_object_name = 'profiles'
+
