3bd642658b63f2229039526b27cd3d94ac69680e
Alven Chen <alvenie@bu.edu>
2025-10-10 03:11:05 -0400

    Updated photo model for mini_insta

--- mini_insta/models.py
+++ mini_insta/models.py
@@ -50,6 +50,27 @@ class Photo(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     image_url = models.URLField(blank=True)
     timestamp = models.DateTimeField(auto_now=True)
+    image_file = models.ImageField(blank=True)
 
     def __str__(self):
-        return f"Photo {self.pk} for Post {self.post.pk} taken at {self.timestamp}"
+        
+        # REVISED __str__ METHOD
+        if self.image_file:
+            source = f"File: {self.image_file.name}"
+        elif self.image_url:
+            source = f"URL: {self.image_url}"
+        else:
+            source = "No Image"
+
+        return f"Photo {self.pk} for Post {self.post.pk} taken at {self.timestamp} | Source: {source}"
+
+    def get_image_url(self):
+        '''Returns the URL to the image'''
+
+        if self.profile_image_url:
+            return self.profile_image_url
+        
+        if self.image_file:
+            return self.image_file
+        
+        return ''
