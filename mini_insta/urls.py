35eaf5a2bfd6e50641e5676b47351fc5310e33c3
alvenie <alvenie.bu.edu>
2026-02-04 01:17:03 -0500

    Fixed logout bug.

--- mini_insta/urls.py
+++ mini_insta/urls.py
@@ -35,7 +35,7 @@ urlpatterns = [
 
     ## authorization related urls
     path('login/', auth_views.LoginView.as_view(template_name='mini_insta/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='logout_confirmation'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='mini_insta:logout_confirmation'), name='logout'),
     path('logout_confirmation/', LogoutConfirmationView.as_view(), name='logout_confirmation'),
     path('create_profile/', CreateProfileView.as_view(), name='create_profile'),
 ]
